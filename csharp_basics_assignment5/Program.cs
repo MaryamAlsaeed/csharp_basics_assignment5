248b9898890b18ce4f3786eea9a94193f7d5d670
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-09 11:31:41 +0400

    Add project files.

---
+++ csharp_basics_assignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace csharp_basics_assignment5
+{
+    class Book
+    {
+        private string password = "secret";
+        internal int copiesInStock = 5;
+        public string Title;
+        public Genre Genre { get; set; }
+    }
+    enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    };
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
